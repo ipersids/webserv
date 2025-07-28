10517baca236be47b99c1cd078798b0fb1909062
Tonttu84 <150278718+Tonttu84@users.noreply.github.com>
2025-07-28 19:23:33 +0300

    Fixed the compiler errors (#3)

--- srcs/data.cpp
+++ srcs/data.cpp
@@ -6,11 +6,11 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 14:00:20 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/07/18 14:56:55 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/07/26 11:48:01 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/webserv.hpp"
+#include "data.hpp"
 
 Data& Data::getData() {
     static Data instance;
